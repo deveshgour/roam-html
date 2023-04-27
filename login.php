a80ab6f392631c4ca8e05e5c6ed30e3ba15e00ae
rahulhencha <70096364+rahulhencha@users.noreply.github.com>
2023-04-27 23:09:09 +0530

    changes

--- login.php
+++ login.php
@@ -10,7 +10,30 @@
 <body>
     <main class="mainContent">
         <div class="authPage">
-
+            <div class="authPage_top text-center">
+                <a href="javascript:void(0);" class="authPage_button"></a>
+                <a href="javascript:void(0);" class="authPage_logo">
+                    <img src="<?php echo IMAGES_URL ?>/logo.png" alt="logo " class="img-fluid">
+                </a>
+            </div>
+            <section class="loginSec">
+                <div class="loginSec_inner">
+                    <div class="loginSec_txt">
+                        <p class="mb-0">Welcome Back!</p>
+                        <h1 class="mb-0">Login</h1>
+                    </div>
+                    <form class="commonForm">
+                        <div class="form-group floating-group">
+                            <label class="floating-label">Email</label>
+                            <input type="email" class="form-control floating-control" />
+                        </div>
+                        <div class="form-group floating-group">
+                            <label class="floating-label">Password</label>
+                            <input type="password" class="form-control floating-control" />
+                        </div>
+                    </form>
+                </div>
+            </section>
         </div>
     </main>
 </body>
