08c322d83bbe0fb623375c552950f9c5e32ba2a2
rahulhencha <70096364+rahulhencha@users.noreply.github.com>
2023-04-28 00:17:00 +0530

    worked on login signup

--- sign-up.php
+++ sign-up.php
@@ -3,7 +3,7 @@
 <?php $currentPage = 'Home'; ?>
 
 <head>
-    <title>Roam | Login</title>
+    <title>Roam | Sign-up</title>
     <?php include 'include/head-link.php' ?>
 </head>
 
@@ -19,8 +19,8 @@
             <section class="loginSec">
                 <div class="loginSec_inner">
                     <div class="loginSec_txt">
-                        <p class="mb-0">Welcome Back!</p>
-                        <h1 class="mb-0">Login</h1>
+                        <p class="mb-0">Create your RoamTag Account</p>
+                        <h1 class="mb-0">Register</h1>
                     </div>
                     <form class="commonForm">
                         <div class="form-group floating-group">
@@ -28,18 +28,31 @@
                             <input type="email" class="form-control floating-control" />
                         </div>
                         <div class="form-group floating-group">
+                            <label class="floating-label">Confirm Email</label>
+                            <input type="email" class="form-control floating-control" />
+                        </div>
+                        <div class="form-group floating-group">
+                            <label class="floating-label">First Name</label>
+                            <input type="text" class="form-control floating-control" />
+                        </div>
+                        <div class="form-group floating-group">
+                            <label class="floating-label">Last Name</label>
+                            <input type="text" class="form-control floating-control" />
+                        </div>
+                        <div class="form-group floating-group">
                             <label class="floating-label">Password</label>
                             <input type="password" class="form-control floating-control" id="password-field"/>
                             <em class="icon-eye form-icon toggle-password" toggle="#password-field"></em>
                         </div>
-                        <div class="text-end form-group">
-                            <a href="javascript:void(0);" class="linkBtn">Forgot Password</a>
+                        <div class="form-group floating-group">
+                            <label class="floating-label">Confirm Password</label>
+                            <input type="password" class="form-control floating-control" id="password-field1"/>
+                            <em class="icon-eye form-icon toggle-password" toggle="#password-field1"></em>
                         </div>
-                        <button class="btn btn-primary w-100">Login</button>
+                        <button class="btn btn-primary w-100">Register</button>
                     </form>
                     <div class="loginSec_bottom text-center">
-                        <p>Don’t have an account? <a href="<?php echo BASE_URL?>/sign-up.php" class="linkBtn">Register here</a> </p>
-                        <p class="mb-0">Forgot your password ? <a href="javascript:void(0);" class="linkBtn">Reset it here</a> </p>
+                        <p class="mb-o">Already have an account?  <a href="<?php echo BASE_URL?>/login.php" class="linkBtn">Login Here</a> </p>
                     </div>
                 </div>
             </section>
