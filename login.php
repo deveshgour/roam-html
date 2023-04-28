18831f962e3c2fa4a8c9fb448f743378e24b80b0
rahulhencha <70096364+rahulhencha@users.noreply.github.com>
2023-04-28 22:12:04 +0530

    worked on login

--- login.php
+++ login.php
@@ -10,8 +10,8 @@
 <body>
     <main class="mainContent">
         <div class="authPage">
-            <div class="authPage_top text-center">
-                <a href="javascript:void(0);" class="authPage_button"></a>
+            <div class="authPage_top text-sm-center">
+                <a href="javascript:void(0);" class="authPage_button d-flex align-items-center justify-content-center"><em class="icon-menu-icon"></em></a>
                 <a href="javascript:void(0);" class="authPage_logo">
                     <img src="<?php echo IMAGES_URL ?>/logo.png" alt="logo " class="img-fluid">
                 </a>
@@ -30,7 +30,7 @@
                         <div class="form-group floating-group">
                             <label class="floating-label">Password</label>
                             <input type="password" class="form-control floating-control" id="password-field"/>
-                            <em class="icon-eye form-icon toggle-password" toggle="#password-field"></em>
+                            <em class="icon-eye-off form-icon toggle-password" toggle="#password-field"></em>
                         </div>
                         <div class="text-end form-group">
                             <a href="javascript:void(0);" class="linkBtn">Forgot Password</a>
@@ -46,89 +46,6 @@
         </div>
     </main>
     <?php include 'include/footer-scripts.php' ?>
-    <script>
 
-    $(".toggle-password").click(function() {
-        $(this).toggleClass("icon-eye icon-eye");
-        var input = $($(this).attr("toggle"));
-        if (input.attr("type") == "password") {
-        input.attr("type", "text");
-        } else {
-        input.attr("type", "password");
-        }
-    })
-    // ----floatin input label in input and select
-
-$('.form-group').find('.floating-control').each(function (index, ele) {
-	var $ele = $(ele);
-	if($ele.val() != '' || $ele.is(':selected') === true){
-		$ele.parents('.floating-group').addClass('focused');
-	}
-})
-
-
-$('.floating-control').on('focus', function (e) {
-	$(this).parents('.floating-group').addClass('focused');	
-}).on('blur', function(){
-	if($(this).val().length > 0){
-		$(this).parents('.floating-group').addClass('focused');		
-	}else{
-		$(this).parents('.floating-group').removeClass('focused');
-	}
-});
-$('.floating-control').on('change', function (e) {
-	if($(this).is('select')){
-		if($(this).val() === $("option:first", $(this)).val()) {
-			$(this).parents('.floating-group').removeClass('focused');
-		}
-		else{
-			$(this).parents('.floating-group').addClass('focused');
-		}
-	}
-})
-
-
-// --------select2-------
-$(document).ready(function() {
-	//---- select2 single----
-	$('.customSelect').each(function() {
-		var dropdownParents = $(this).parents('.select2Part')
-		$(this).select2({
-			dropdownParent: dropdownParents,
-			minimumResultsForSearch: -1
-		}).on("select2:open", function (e) { 
-			$(this).parents('.floating-group').addClass('focused');
-		}).on("select2:close", function (e) {
-			if($(this).find(':selected').val() === ''){
-				$(this).parents('.floating-group').removeClass('focused');
-			}
-		});
-	});
-
-	//---- select2 multiple----
-	$('.customSelectMultiple').each(function() {
-		var dropdownParents = $(this).parents('.select2Part');
-		// var placehldrget = $(this).attr("data-placeholder");
-		$(this).select2({
-			dropdownParent: dropdownParents,
-			// placeholder: placehldrget,
-			// tags: true,
-			// closeOnSelect: false,
-		}).on("select2:open", function (e) { 
-			$(this).parents('.floating-group').addClass('focused');
-		}).on("select2:close", function (e) {
-			if($(this).val() != ''){
-				$(this).parents('.floating-group').addClass('focused');
-			}else{
-				$(this).parents('.floating-group').removeClass('focused');
-			}
-		}).on("select2:select", function (e) { 
-			$(this).parents('.floating-group').addClass('focused');
-		}).on("select2:unselect", function (e) {
-			$(this).parents('.floating-group').addClass('focused');
-		})
-	});
-});
-</script>
 </body>
 </html>
