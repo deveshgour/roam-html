07b8580429084c80dd25a79f44ad1efc2b4f4c52
devesh gour <deveshgour98@gmail.com>
2023-04-28 21:13:41 +0530

    update icons and header

---
+++ include/header.php
@@ -0,0 +1,23 @@
+<header class="header">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="<?php echo IMAGES_URL ?>/logo.png" class="img-fluid" alt="logo"/>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                        <em class="icon-bell-icon"></em>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> <em class="icon-menu-icon"></em></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
--- pet-list.php
+++ pet-list.php
@@ -12,19 +12,48 @@
     <?php include('include/header.php'); ?>
     <main class="petListPage">
 
-        <div class="container">
-            <section class="petListBanner">
-                <h1>The lat <span class="font-bd">Pet Tag </span> <br> You'll ever need</h1>
-                <p>We took Apple's Find My tracking technology <br> and built it into a pet tag.</p>
-                <a class="btn btn-secondary" href="#">Shop Now</a>
-            </section>    
-        </div>
 
-    
+        <section class="petListBanner">
+            <div class="container">
+                <div class="petListBanner_inner">
+                    <h1>The lat <span class="font-bd">Pet Tag </span> <br> You'll ever need</h1>
+                    <p>We took Apple's Find My tracking technology <br> and built it into a pet tag.</p>
+                    <a class="btn btn-secondary" href="#">Shop Now</a>
+                </div>
+            </div>
+        </section>
+        <section class="myPets">
+            <div class="container">
+                <div class="myPets_inner">
+                    <h2>My Pet’s</h2>
+                    <p>Tap on a pet to view their profile</p>
+
+                    <ul class="list-unstyled">
+                        <li class="myPets_item">
+                            <div class="myPets_item_img">
+                                <img src="<?php echo IMAGES_URL ?>/pet_1.png" alt="pet" class="img-fluid" />
+                            </div>
+                            <p>Jebby</p>
+                        </li>
+                        <li class="myPets_item myPets_item-add">
+                            <a href="javascript:;">
+                                <div class="myPets_item_icon">
+                                    <img src="<?php echo IMAGES_URL ?>/add_circle_icon.svg" alt="add" class="img-fluid" />
+                                </div>
+                                <p>Add Pet</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+
+
     </main>
-    <?php include ('include/footer.php') ?>
-    <?php include ('include/footer-scripts.php') ?>
-  
+    <?php include('include/footer.php') ?>
+    <?php include('include/footer-scripts.php') ?>
+
 </body>
 
 </html>
