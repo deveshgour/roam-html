bf6132cdc25cd33ee8b21169699a5eef440d6129
rahulhencha <70096364+rahulhencha@users.noreply.github.com>
2023-04-29 20:40:53 +0530

    add-new-pet.html

--- add-new-pet.php
+++ add-new-pet.php
@@ -14,11 +14,11 @@
         <div class="container">
             <div class="addNewPetPage_inner">
                 <a href="javascript:void(0)" class="backBtn"><em class="icon-arrow-left-icon"></em> Add New Pet</a>
-                <div class="addNewPetPage_details">
+                <div class="addNewPetPage_details py-60 pt-0">
                     <section class="codeSec">
                         <div class="codeSec_inner">
                             <h3>Enter the code on the back of your RoamTag. </h3>
-                            <p>If the link on your RoamTag is RoamTag.co/tag/aBc123, your <b>code is aBc123</b>.</p>
+                            <p class="mb-0">If the link on your RoamTag is RoamTag.co/tag/aBc123, your <b>code is aBc123</b>.</p>
                         </div>
                     </section>
                     <section class="fromSec">
@@ -30,17 +30,17 @@
                             <p class="formNote">Please enter a Tag code.</p>
                         </form>
                     </section>
-                    <section class="orSec">
-                        
-                    </section>
-                    <section class="codeSec">
-                        <div class="codeSec_inner d-flex align-items-center">
-                            <div class="codeSec_txt">
+                    <div class="orImage text-center">
+                        <img src="<?php echo IMAGES_URL ?>/or-img.png" alt="or-img" class="img-fluid" />
+                    </div>
+                    <section class="scan">
+                        <div class="scan_inner d-flex align-items-center">
+                            <div class="scan_txt">
                                 <p>Scan the QR code on the back of Tag, Using of your back camera. </p>
-                                <span>Scan Now</span>
+                                <p class="mb-0"><b>Scan Now</b></p>
                             </div>
-                            <div class="codeSec_img">
-                                
+                            <div class="scan_img flex-shrink-0">
+                                <img src="<?php echo IMAGES_URL ?>/scan-img.png" alt="scan-img" class="img-fluid" />
                             </div>
                         </div>
                     </section>
